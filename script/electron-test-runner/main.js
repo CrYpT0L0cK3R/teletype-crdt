317150243ff56c56c04bff1de87c91a43af20036
Matthew Thompson <dont6tread6on6me@gmail.com>
2020-04-29 00:10:41 -0500

    Revert "Fix vulnerabilities"
    This reverts commit 508688fdcebb7b398638740b534fb92a648aae95.

--- script/electron-test-runner/main.js
+++ script/electron-test-runner/main.js
@@ -10,11 +10,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {nodeIntegration: true}
-  })
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
